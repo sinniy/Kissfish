e2570373bb27809fd214a76c50e7a96e9605dc1a
sinniy <heng9yuan@gmail.com>
2017-03-24 15:06:45 +0800

    Test

--- KissfishWeb/src/main/java/com/kissfish/app/Test.java
+++ KissfishWeb/src/main/java/com/kissfish/app/Test.java
@@ -4,4 +4,11 @@ package com.kissfish.app;
  * Created by Lixin on 2016/1/7.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("Vodi main......");
+    }
+
+    public void m(){
+
+    }
 }
