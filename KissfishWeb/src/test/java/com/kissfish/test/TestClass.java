4c60b64d6618665723d88a4c5398d13a29ade7fe
sinniy <heng9yuan@gmail.com>
2017-03-24 15:01:01 +0800

    TestClass提交

---
+++ KissfishWeb/src/test/java/com/kissfish/test/TestClass.java
@@ -0,0 +1,15 @@
+package com.kissfish.test;
+
+
+import org.junit.Test;
+
+/**
+ * Created by lixin on 2017/3/22.
+ */
+public class TestClass {
+    @Test
+    public void name() throws Exception {
+
+    }
+
+}
